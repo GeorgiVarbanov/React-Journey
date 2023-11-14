bdde6197ed1589df8f908b71cdcb94894246fef9
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-11-14 23:49:47 +0200

    Add Game component

--- GamesPlay/client/src/componets/Catalogue/Catalogue.jsx
+++ GamesPlay/client/src/componets/Catalogue/Catalogue.jsx
@@ -11,24 +11,6 @@ const Catalogue = () => {
                 </div>
 
             </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                        <h6>Action</h6>
-                        <h2>Zombie lang</h2>
-                        <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                        <h6>Action</h6>
-                        <h2>MineCraft</h2>
-                        <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
-
             <h3 className="no-articles">No articles yet</h3>
         </section>
     )
