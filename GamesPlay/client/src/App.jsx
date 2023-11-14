685c4233c649da9c3c3595fd3b798c1f1ec8afae
GeorgiVarbanov <g.varbanovv@gmail.com>
2023-11-14 21:16:49 +0200

    Set up project with initial React components

---
+++ GamesPlay/client/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "../componets/Header/Header.jsx"
+import Home from "../componets/Home/Home.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  )
+}
+
+export default App;
